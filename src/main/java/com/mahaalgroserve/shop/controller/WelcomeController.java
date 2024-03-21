374a369b8ab1849182f615c8549c2455dd2805eb
Majutharan <majutharan@seedbyte.io>
2024-03-21 22:19:33 +0800

    added welcome controller

---
+++ src/main/java/com/mahaalgroserve/shop/controller/WelcomeController.java
@@ -0,0 +1,17 @@
+package com.mahaalgroserve.shop.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping(value = "/welcome", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String welcomeAsHTML() {
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
+    }
+}
